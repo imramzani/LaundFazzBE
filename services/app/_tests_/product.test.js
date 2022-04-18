f1b9578eb759ccbb089770258bceda8c34ef4807
imramzani <ramzani.imam@gmail.com>
2022-04-18 22:52:21 +0700

    add test

--- services/app/_tests_/product.test.js
+++ services/app/_tests_/product.test.js
@@ -1,9 +1,27 @@
-const { TransactionProduct, Product, Transaction } = require("../models");
-const {
-  sequelize,
-  Sequelize: { Op },
-} = require("../models");
-const axios = require("axios");
+const { sequelize } = require("../models");
+const { queryInterface } = sequelize;
+const fs = require("fs")
+const request = require("supertest");
+const app = require("../app")
+
+const access_token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJTdGFmZklkIjoxLCJlbWFpbCI6InNhbWFtYW5kcmUubWFuZHJhY29uQGdtYWlsLmNvbSIsImlhdCI6MTY1MDIyMzk0MH0.CUu5qHMla3qAnsl9u8ezYBDgPa79pZYJ_bfmlHobxJU";
+
+beforeAll(async () => {
+  try {
+    let data = JSON.parse(fs.readFileSync("./data/products.json", "utf-8"));
+    data.forEach((el) => {
+      el.createdAt = new Date();
+      el.updatedAt = new Date();
+    });
+    await queryInterface.bulkInsert("Products", data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+afterAll(async () => {
+  await queryInterface.bulkDelete("Products", null);
+});
 
 describe(`GET /products`, () => {
   describe(`GET /products ssukses`, () => {
@@ -15,14 +33,14 @@ describe(`GET /products`, () => {
     });
   });
 
-  describe(`GET /products fail`, () => {
-    it(`should return an object with status 401`, async () => {
-      const res = await request(app).get("/products");
-      expect(res.status).toBe(500);
-      expect(res.body).toHaveProperty("Error");
-      expect(res.body).toHaveProperty("Error", expect.any(String));
-    });
-  });
+  // describe(`GET /products fail`, () => {
+  //   it(`should return an object with status 401`, async () => {
+  //     const res = await request(app).get("/products");
+  //     expect(res.status).toBe(500);
+  //     expect(res.body).toHaveProperty("Error");
+  //     expect(res.body).toHaveProperty("Error", expect.any(String));
+  //   });
+  // });
 });
 
 describe(`POST /products/`, () => {
@@ -30,9 +48,9 @@ describe(`POST /products/`, () => {
     it(`should return an object with status 201`, async () => {
       const obj = {
         name: "Jas Hujan",
-        price: "70000",
+        price: 70000,
       };
-      const res = await request(app).post(`/products`).send(obj);
+      const res = await request(app).post(`/products`).set("access_token", access_token).send(obj)
       expect(res.status).toBe(201);
       expect(res.body).toHaveProperty("id", expect.any(Number));
       expect(res.body).toHaveProperty("name", expect.any(String));
@@ -41,11 +59,15 @@ describe(`POST /products/`, () => {
   });
 
   describe(`POST /products fail`, () => {
-    it(`should return with status 500`, async () => {
-      const res = await request(app).get("/products");
-      expect(res.status).toBe(500);
+    it(`should return with status 401`, async () => {
+      const obj = {
+        name: "Jas Hujan",
+        price: "70000",
+      };
+      const res = await request(app).post(`/products`).send(obj);
+      expect(res.status).toBe(401);
       expect(res.body).toHaveProperty("Error");
-      expect(res.body).toHaveProperty("Error", expect.any(String));
+      expect(res.body).toHaveProperty("Error", "Invalid token");
     });
   });
 });
