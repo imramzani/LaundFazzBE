e76c7d16c010042c89b5654f638a32e138b7a47a
mwbintang <94796835+mwbintang@users.noreply.github.com>
2022-04-19 21:26:33 +0700

    debug again

--- services/app/bin/www.js
+++ services/app/bin/www.js
@@ -1,10 +1,10 @@
-const {app, server, PORT} = require("../app")
-const portApp = process.env.PORT || 3000;
+const app = require("../app")
+const PORT = process.env.PORT || 3000;
 
-app.listen(portApp, () => {
-  console.log(`Listening to PORT ${portApp}`);
+app.listen(PORT, () => {
+  console.log(`Listening to PORT ${PORT}`);
 });
 
-server.listen(PORT, () => {
-  console.log(`server on port ${PORT}`)
-})
+// server.listen(PORT, () => {
+//   console.log(`server on port ${PORT}`)
+// })
